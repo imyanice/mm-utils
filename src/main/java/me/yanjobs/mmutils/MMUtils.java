d3a0e53712f0d58e4060f2269f577aa9bd13c4b6
Yanice <git@yanice.me>
2026-04-19 13:02:30 +0200

    bump to 2.0.0

--- src/main/java/me/yanjobs/mmutils/MMUtils.java
+++ src/main/java/me/yanjobs/mmutils/MMUtils.java
@@ -12,7 +12,7 @@ import net.weavemc.api.event.EventBus;
 
 public class MMUtils implements ModInitializer {
     public static boolean isInMMClassic;
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "2.0.0";
     private static Config config;
 
     @Override
